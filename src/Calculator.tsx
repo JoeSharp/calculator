89f61ea70d1f2b996f0b9f3c842a86bbfdcc8190
Joe Sharp <ratracejoe@hotmail.com>
2022-01-23 16:40:23 +0000

    commenting various blocks, removing redundant definition of button props, fixing divide multiply bug

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,6 +1,9 @@
-import React, { MouseEventHandler } from 'react';
+import React from 'react';
 import './calculator.css';
 
+/**
+ * These are the supported operations in our calculator
+ */
 enum Operation {
   add = '+',
   subtract = '-',
@@ -8,59 +11,99 @@ enum Operation {
   multiply = '*'
 }
 
+/**
+ * A function that executes a two operand operation
+ * @param operation The operation to execute
+ * @param a First operand
+ * @param b Second operand
+ * @returns The result of the operation
+ */
 const executeOperation = (operation: Operation, a: number, b: number): number => {
   switch (operation) {
     case Operation.add: return a + b;
     case Operation.subtract: return a - b;
-    case Operation.divide: return a * b;
-    case Operation.multiply: return a / b;
+    case Operation.divide: return a / b;
+    case Operation.multiply: return a * b;
   }
 }
 
+/**
+ * A generalised form of the actions that can be sent to our reducer
+ */
 interface AbstractCalculatorAction {
   type: string;
 }
 
+/**
+ * When the user clicks on one of the number buttons
+ */
 interface AppendNumberAction extends AbstractCalculatorAction {
   type: 'appendNumber',
   digit: number
 }
 
+/**
+ * The user has clicked on the delete button
+ */
 interface DeleteDigitAction extends AbstractCalculatorAction {
   type: 'deleteDigit'
 }
 
+/**
+ * User clicks on the decimal point button
+ */
 interface AddDecimalPointAction extends AbstractCalculatorAction {
   type: 'addDecimalPoint'
 }
 
+/**
+ * User clicks on one of the operator buttons
+ */
 interface ChooseOperation extends AbstractCalculatorAction {
   type: 'chooseOperation',
   operation: Operation
 }
 
+/**
+ * User clicks on equals
+ */
 interface ComputeAction extends AbstractCalculatorAction {
   type: 'compute'
 }
 
+/**
+ * User clicks on the clear button (AC)
+ */
 interface ClearAction extends AbstractCalculatorAction {
   type: 'clear'
 }
 
 type CalculatorAction = AppendNumberAction | DeleteDigitAction | AddDecimalPointAction | ChooseOperation | ComputeAction | ClearAction;
 
+/**
+ * The state of the calculator
+ */
 interface CalculatorState {
   currentOperand: string;
   previousOperand: string;
   operation: Operation | undefined
 }
 
+/**
+ * The clear down state of the calculator
+ */
 const DEFAULT_CALCULATOR_STATE: CalculatorState = {
   currentOperand: '',
   previousOperand: '',
   operation: undefined
 }
 
+/**
+ * Executes the current operation.
+ * 
+ * @param state The current calculator state
+ * @returns The new calculator state
+ */
 const compute = (state: CalculatorState): CalculatorState => {
   const prev = parseFloat(state.previousOperand)
   const current = parseFloat(state.currentOperand)
@@ -75,6 +118,13 @@ const compute = (state: CalculatorState): CalculatorState => {
   }
 }
 
+/**
+ * This reducer executes the various actions of the calculator.
+ * 
+ * @param state The current state of the calculator
+ * @param action When the user clicks on a button, it will generally trigger an action
+ * @returns The new state
+ */
 const calculatorReducer = (state: CalculatorState, action: CalculatorAction): CalculatorState => {
   switch (action.type) {
     case 'appendNumber':
@@ -112,31 +162,31 @@ const calculatorReducer = (state: CalculatorState, action: CalculatorAction): Ca
   }
 }
 
-interface ButtonProps {
-  onClick: MouseEventHandler,
-  className?: string
-  text: string
-}
-
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = React.useReducer(calculatorReducer, DEFAULT_CALCULATOR_STATE);
 
-  const createNumberButtonProps = React.useCallback((digit: number): ButtonProps => ({
-    text: digit.toString(10),
+  // Buttons that add digits have a common shape
+  const createNumberButtonProps = React.useCallback((digit: number): React.HTMLProps<HTMLButtonElement> => ({
+    value: digit.toString(10),
     onClick: () => dispatch({ type: 'appendNumber', digit })
-  }), [])
-  const createOperationButtonProps = React.useCallback((operation: Operation): ButtonProps => ({
-    text: operation,
+  }), []);
+
+  // Buttons that trigger operations have a common shape
+  const createOperationButtonProps = React.useCallback((operation: Operation): React.HTMLProps<HTMLButtonElement> => ({
+    value: operation,
     onClick: () => dispatch({ type: 'chooseOperation', operation })
   }), [])
 
-  const buttons: ButtonProps[] = React.useMemo(() => {
+  /**
+   * Create an array of button properties that we can then iterate through
+   */
+  const buttons: React.HTMLProps<HTMLButtonElement>[] = React.useMemo(() => {
     return [{
-      text: 'AC',
+      value: 'AC',
       className: 'span-two',
       onClick: () => dispatch({ type: 'clear' })
     }, {
-      text: 'DEL',
+      value: 'DEL',
       onClick: () => dispatch({ type: 'deleteDigit' })
     },
     createOperationButtonProps(Operation.divide),
@@ -153,11 +203,11 @@ function App() {
     createNumberButtonProps(9),
     createOperationButtonProps(Operation.subtract),
     {
-      text: '.',
+      value: '.',
       onClick: () => dispatch({ type: 'addDecimalPoint' })
     },
     createNumberButtonProps(0), {
-      text: '=',
+      value: '=',
       className: 'span-two',
       onClick: () => dispatch({ type: 'compute' })
     }]
@@ -169,8 +219,8 @@ function App() {
         <div data-previous-operand className="previous-operand">{previousOperand} {operation}</div>
         <div data-current-operand className="current-operand">{currentOperand}</div>
       </div>
-      {buttons.map(({ onClick, text, className }) =>
-        <button className={className} onClick={onClick}>{text}</button>
+      {buttons.map(({ onClick, value, className }) =>
+        <button className={className} onClick={onClick}>{value}</button>
       )}
     </div>
   );
